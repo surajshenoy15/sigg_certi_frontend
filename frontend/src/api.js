9bcab67d278c576bfd78b95df8bf06c4173dd019
suraj shenoy <surajshenoyp@gmail.com>
2026-05-17 18:00:56 +0530

    added backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || ''
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://sigg-certi-backend.onrender.com'
 
 export const api = axios.create({
   baseURL: API_BASE,
